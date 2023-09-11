6e4e286a9294b70578634e7293bf7c6161b4d784
Alexandru <alexandru.gurau@live.com>
2023-09-11 18:07:11 +0100

    adding sort and filterig feature for cabins

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -1,4 +1,6 @@
-import styled, { css } from "styled-components";
+/* eslint-disable react/prop-types */
+import { useSearchParams } from 'react-router-dom';
+import styled, { css } from 'styled-components';
 
 const StyledFilter = styled.div`
   border: 1px solid var(--color-grey-100);
@@ -33,3 +35,29 @@ const FilterButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
+
+function Filter({ filterField, options }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentFilter = searchParams.get(filterField) || options.at(0).value;
+
+  const handleClick = (value) => {
+    searchParams.set(filterField, value);
+    setSearchParams(searchParams);
+  };
+
+  return (
+    <StyledFilter>
+      {options.map((el) => (
+        <FilterButton
+          key={el.value}
+          onClick={() => handleClick(el.value)}
+          active={el.value === currentFilter ? currentFilter : ''}
+          disabled={el.value === currentFilter}>
+          {el.label}
+        </FilterButton>
+      ))}
+    </StyledFilter>
+  );
+}
+
+export default Filter;
