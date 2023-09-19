867fe485f189eb1aedb41508a42a0d5545546bff
Alexandru <alexandru.gurau@live.com>
2023-09-19 12:00:44 +0100

    finish dashboard, fixing some small bugs and more

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallback from './ui/ErrorFallback.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => window.location.replace('/')}>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
+
+// ErrorBoundary for this I need more documentation ;
+// Basicaly it catch errors only when react render, for other error we have to use other mechanisms
